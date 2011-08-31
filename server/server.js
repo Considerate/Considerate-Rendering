f394088a35f6da3f460947af573aa683347e1120
Viktor Kronvall <viktor.kronvall@gmail.com>
2011-09-01 01:55:30 +0200

    Added web server using node.js

---
+++ server/server.js
@@ -0,0 +1,4 @@
+var webHost = require("./web");
+var port = 3000;
+webHost.app.listen(3000);
+console.log('example running on port '+port)
---
+++ server/web.js
@@ -0,0 +1,31 @@
+var express = require('express')
+, app = express.createServer();
+
+// config
+app.configure(function(){
+    console.log(__dirname);
+    app.set('views', __dirname + '/../views');
+    app.set('view engine', 'mustache');
+    app.register(".mustache", require('stache'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/../public'));
+    /*app.use(require('browserify')({
+        require : __dirname + '/../shared/main'
+    }));*/
+});
+
+// routes
+app.get('/', function (req, res) {
+    res.render('index', {
+        locals: {
+            title: 'Welcome',
+            page:{
+                main: true
+            }
+        }
+    });
+});
+
+exports.app = app;
