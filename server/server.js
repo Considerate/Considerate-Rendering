2d66d6d87094383d46c5fe3a03c01cd01d61bfa1
Viktor Kronvall <viktor.kronvall@gmail.com>
2011-09-04 01:01:51 +0200

    Restored server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,4 @@
-/*var webHost = require("./web");
+var webHost = require("./web");
 var port = process.env.PORT || 3000;
 webHost.app.listen(3000);
-console.log('example running on port '+port)*/
-
-
-var http = require('http');
- 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n");
-});
- 
-server.listen(process.env.PORT || 8001);
+console.log('example running on port '+port)
