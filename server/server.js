91bb7d77a88b8bd762dc30fac74e58663ed4f819
Viktor Kronvall <viktor.kronvall@gmail.com>
2011-09-04 00:53:58 +0200

    Test

--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
-var webHost = require("./web");
+/*var webHost = require("./web");
 var port = process.env.PORT || 3000;
 webHost.app.listen(3000);
-console.log('example running on port '+port)
+console.log('example running on port '+port)*/
+
+
+var http = require('http');
+ 
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("Hello world\n");
+});
+ 
+server.listen(process.env.PORT || 8001);
